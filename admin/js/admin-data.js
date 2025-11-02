8fba56feea4712239ef808f85a58f1b2b2217cd5
cyberdude-de <sven.nitsche.on@gmail.com>
2025-11-02 18:38:43 +0100

    Update admin-data.js

--- admin/js/admin-data.js
+++ admin/js/admin-data.js
@@ -1,1 +1,11 @@
-
+// Beispiel: Holt Daten aus veröffentlichtem Google Sheet als JSON
+fetch("DEINE_JSON_FEED_URL")
+  .then(res => res.json())
+  .then(data => {
+    const tbody = document.querySelector("#teamTable tbody");
+    data.forEach(row => {
+      const tr = document.createElement("tr");
+      tr.innerHTML = `<td>${row.verein}</td><td>${row.teamname}</td><td>${row.altersklasse}</td><td>${row.kinderzahl}</td><td>${row.kontaktperson}</td>`;
+      tbody.appendChild(tr);
+    });
+  });
